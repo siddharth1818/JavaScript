c2646334b8cc0f3cca7bf05bd29b91e4ac0f0c02
Siddharth Ahire <siddharthahire143@gmail.com>
2024-04-21 20:07:22 +0530

    product store project added

---
+++ product_store/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import { Product } from './components/Product';
+import { Cart } from './components/Cart';
+
+function App() {
+  return (
+    <>
+      <Product/>
+    </>
+  );
+}
+
+export default App;
